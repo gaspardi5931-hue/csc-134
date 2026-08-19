6768b40c6a01248a6781080886e33fc1e799957b
gaspardi5931-hue <gaspardi5931@student.faytechcc.edu>
2026-08-19 11:09:35 -0400

    uploaded M1T1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// One line comment
+/*
+Block comment
+M1T1 - Hello World
+Ivan
+8/19/26
+First program - just a smoke test
+*/
+
+// magic words to start up
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "HELLO CSC 134!" << endl;
+    string name;
+    cout << "What's your name? ";
+    cin >> name; // input -reads th name into variable
+    cout << "This program is for : " << name << endl;
+    return 0; // no errors
+}
