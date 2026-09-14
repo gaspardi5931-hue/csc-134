f6d95ae8c556dbea533664a22287f945a90096e7
gaspardi5931-hue <gaspardi5931@student.faytechcc.edu>
2026-09-14 15:07:57 +0000

    done

--- m2t2_090x.cpp
+++ m2t2_090x.cpp
@@ -32,7 +32,13 @@ int main () {
 
     // Print the receipt 
     cout << setprecision (2)<< fixed;
-    cout << total << endl;
+    cout << "Thank you for shopping with us" << endl;
+    cout << "------------------------------" << endl;
+    cout << item << "\t$" << item_price      << endl;
+    cout << "Tax " << "\t\t$" << tax_amount    << endl;
+    cout << "------------------------------" << endl;
+    cout << "Total" << "\t\t$" << total << endl;
+    cout << endl;
 
     return 0; // no errors
 }
